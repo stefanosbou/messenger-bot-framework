0858a500217463e5c9ecbe702ea5858fec5184af
stefanos <stefanos.bou@gmail.com>
2016-08-04 11:16:33 +0300

    Server verticle subscribe endpoint completed with tests

--- src/main/java/io/github/stefanosbou/server/Server.java
+++ src/main/java/io/github/stefanosbou/server/Server.java
@@ -1,5 +1,9 @@
 package io.github.stefanosbou.server;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.MultiMap;
 import io.vertx.ext.web.Router;
@@ -7,33 +11,49 @@ import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
 
 public class Server extends AbstractVerticle{
+	private static final String CONFIG_FILE = "resources/config.properties";
+	Properties properties;
 	
 	@Override
 	public void start() {
+		loadProperties(CONFIG_FILE);
 		
 	    Router router = Router.router(vertx);
-
+	    
+	    String ENDPOINT = properties.getProperty("endpoint");
+	    
 	    router.route().handler(BodyHandler.create());
-	    router.get("/").handler(this::handleWebhookVerification);
+	    router.get(ENDPOINT).handler(this::handleWebhookVerification);
 //	    router.post(ENDPOINT).handler(this::processRequest);
 
-	    vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+	    int port = Integer.parseInt(properties.getProperty("port"));
+	    vertx.createHttpServer().requestHandler(router::accept).listen(port);
 	}
 	
+	private void loadProperties(String file) {
+		properties = new Properties();
+		try {
+			properties.load(new FileInputStream(file));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	private void handleWebhookVerification(RoutingContext ctx){
+		String VERIFY_TOKEN = properties.getProperty("verify_token");
 		String res;
 		MultiMap params = ctx.request().params();
-		if(params != null && ctx.request().getParam("hub.mode").equals("subscribe")){
+		if(!params.isEmpty() && ctx.request().getParam("hub.mode").equals("subscribe")){
 			String verifyToken = ctx.request().getParam("hub.verify_token");
 			String challenge = ctx.request().getParam("hub.challenge");
-			if (verifyToken.equals("VERIFY_TOKEN") && !challenge.isEmpty()) {
+			if (verifyToken.equals(VERIFY_TOKEN) && !challenge.isEmpty()) {
 			      res = challenge;
 			} else {
-				res = null;
+				res = "WRONG_VERIFY_TOKEN_FOUND_IN_REQUEST";
 				System.out.println("Wrong verify token found in request");
 			}
 		}else{
-			res = null;
+			res = "EMPTY_OR_BAD_REQUEST";
 			System.out.println("Exception no verify token found in request");
 		}
 		ctx.response().end(res);
--- src/test/java/io/github/stefanosbou/server/ServerTest.java
+++ src/test/java/io/github/stefanosbou/server/ServerTest.java
@@ -1,5 +1,9 @@
 package io.github.stefanosbou.server;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import io.vertx.core.Vertx;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
@@ -13,24 +17,35 @@ import org.junit.runner.RunWith;
 @RunWith(VertxUnitRunner.class)
 public class ServerTest {
 	private Vertx vertx;
+	Properties properties;
 	
 	@Before
 	public void setUp(TestContext context) {
+		properties = new Properties();
+		try {
+			properties.load(new FileInputStream(
+			        "resources/config.properties"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	    vertx = Vertx.vertx();
 	    vertx.deployVerticle(Server.class.getName(),
 	        context.asyncAssertSuccess());
 	}
 	
-	@After
-	public void tearDown(TestContext context) {
-	    vertx.close(context.asyncAssertSuccess());
-	}
-	
 	@Test
-	public void testMyApplication(TestContext context) {
+	public void testServerSuccess(TestContext context) {
 	    final Async async = context.async();
-	    String payload = "hub.mode=subscribe&hub.verify_token=VERIFY_TOKEN&hub.challenge=12345";
-	    vertx.createHttpClient().get(8080, "localhost", "/?"+payload,
+	    String ENDPOINT = properties.getProperty("endpoint");
+	    String VERIFY_TOKEN = properties.getProperty("verify_token");
+	    String CHALLENGE = "123456";
+	    int port = Integer.parseInt(properties.getProperty("port"));
+	    
+	    String payload = "/?hub.mode=subscribe";
+	    payload += "&hub.verify_token="+VERIFY_TOKEN;
+	    payload += "&hub.challenge="+CHALLENGE;
+	    
+	    vertx.createHttpClient().get(port, "localhost", ENDPOINT+payload,
 	     response -> {
 	      response.handler(body -> {
 	        context.assertTrue(body.toString().contains("12345"));
@@ -38,4 +53,62 @@ public class ServerTest {
 	      });
 	    }).end();
 	}
+	@Test
+	public void testServerWrongVerifyToken(TestContext context) {
+	    final Async async = context.async();
+	    String ENDPOINT = properties.getProperty("endpoint");
+	    String VERIFY_TOKEN = properties.getProperty("verify_token");
+	    String WRONG_TOKEN = VERIFY_TOKEN + "NOT";
+	    String CHALLENGE = "123456";
+	    int port = Integer.parseInt(properties.getProperty("port"));
+	    
+	    String payload = "/?hub.mode=subscribe";
+	    payload += "&hub.verify_token="+WRONG_TOKEN;
+	    payload += "&hub.challenge="+CHALLENGE;
+	    
+	    vertx.createHttpClient().get(port, "localhost", ENDPOINT+payload,
+	     response -> {
+	      response.handler(body -> {
+	        context.assertTrue(body.toString().contains("WRONG_VERIFY_TOKEN_FOUND_IN_REQUEST"));
+	        async.complete();
+	      });
+	    }).end();
+	}
+	
+	@Test
+	public void testServerEmptyRequest(TestContext context) {
+	    final Async async = context.async();
+	    String ENDPOINT = properties.getProperty("endpoint");
+	    int port = Integer.parseInt(properties.getProperty("port"));
+	    
+	    vertx.createHttpClient().get(port, "localhost", ENDPOINT,
+	     response -> {
+	      response.handler(body -> {
+	        context.assertTrue(body.toString().contains("EMPTY_OR_BAD_REQUEST"));
+	        async.complete();
+	      });
+	    }).end();
+	}
+	
+	@Test
+	public void testServerBadRequest(TestContext context) {
+	    final Async async = context.async();
+	    String ENDPOINT = properties.getProperty("endpoint");
+	    int port = Integer.parseInt(properties.getProperty("port"));
+	    String payload = "/?hub.mode=WRONG_MODE";
+	    
+	    vertx.createHttpClient().get(port, "localhost", ENDPOINT+payload,
+	     response -> {
+	      response.handler(body -> {
+	        context.assertTrue(body.toString().contains("EMPTY_OR_BAD_REQUEST"));
+	        async.complete();
+	      });
+	    }).end();
+	}
+	
+	@After
+	public void tearDown(TestContext context) {
+	    vertx.close(context.asyncAssertSuccess());
+	}
+	
 }
